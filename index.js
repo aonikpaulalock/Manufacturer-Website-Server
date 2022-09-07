122ae28b850cfb4309f8dc8ae8dd0cf52fe0601a
aonikpaulalock <alockpaul9845@gmail.com>
2022-09-07 18:59:39 +0600

    aded ssss

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 4000
-require('dotenv').config()
+const app = express() ;
+const port = process.env.PORT  ||  4000 ;
+require('dotenv').config();
 // const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -14,9 +14,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-
-
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.xaykuto.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -61,13 +58,13 @@ async function run() {
       const result = await paymentCollection.insertOne(payment);
       const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
       res.send(updatedOrder);
-    })
+    });
 
     // Get All Tools
     app.get("/tools", async (req, res) => {
       const result = await toolsCollection.find().toArray();
       res.send(result)
-    })
+    });
 
     // Specifice Id Tools
     app.get("/tool/:id", async (req, res) => {
@@ -75,14 +72,14 @@ async function run() {
       const filter = { _id: ObjectId(id) };
       const tools = await toolsCollection.findOne(filter);
       res.send(tools);
-    })
+    });
 
     // Add Users
     app.post("/tools", async (req, res) => {
       const tools = req.body;
       const result = await toolsCollection.insertOne(tools)
       res.send(result)
-    })
+    });
 
 
     // Delete Manage Product
@@ -91,7 +88,7 @@ async function run() {
       const filter = { _id: ObjectId(id) };
       const result = await toolsCollection.deleteOne(filter);
       res.send(result);
-    })
+    });
 
     // User Order Data
     app.post("/orders", async (req, res) => {
@@ -99,7 +96,7 @@ async function run() {
       // const query = { email: order.email }
       const result = await orderCollection.insertOne(order);
       res.send(result)
-    })
+    });
 
     // User Ordering-Data per User
     app.get("/order", async (req, res) => {
@@ -108,13 +105,13 @@ async function run() {
       const cursor = orderCollection.find(filter);
       const result = await cursor.toArray()
       res.send(result);
-    })
+    });
 
     // Load All User Aded Order
     app.get("/orders", async (req, res) => {
       const result = await orderCollection.find().toArray()
       res.send(result)
-    })
+    });
 
     // Order Data Delete Normal User
     app.delete('/order/:id', async (req, res) => {
@@ -122,7 +119,7 @@ async function run() {
       const filter = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(filter);
       res.send(result);
-    })
+    });
 
 // Payment Order Data
 app.get('/order/:id', async (req, res) => {
@@ -130,20 +127,20 @@ app.get('/order/:id', async (req, res) => {
   const filter = { _id: ObjectId(id) };
   const result = await orderCollection.findOne(filter);
   res.send(result);
-})
+});
 
     // Reviews Get
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray()
       res.send(result)
-    })
+    });
 
     // Post Reviews
     app.post("/review", async (req, res) => {
       const reviews = req.body;
       const result = await reviewCollection.insertOne(reviews);
       res.send(result)
-    })
+    });
 
 
     // Profile Update
@@ -151,7 +148,7 @@ app.get('/order/:id', async (req, res) => {
       const user = req.body;
       const result = await profileCollection.insertOne(user);
       res.send(result)
-    })
+    });
 
 
     // Create User 
@@ -166,7 +163,7 @@ app.get('/order/:id', async (req, res) => {
       // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN)
       const result = await userCollection.updateOne(filter, updateDoc, options);
       res.send(result)
-    })
+    });
 
 
     //  Click Admin Button And Create admin
@@ -180,7 +177,7 @@ app.get('/order/:id', async (req, res) => {
       }
       const result = await userCollection.updateOne(filter, updateDoc)
       res.send(result)
-    })
+    });
 
     // Admin or Not
     app.get("/user/:email", async (req, res) => {
@@ -189,28 +186,28 @@ app.get('/order/:id', async (req, res) => {
       const user = await userCollection.findOne(filter);
       const isAdmin = user.role === 'admin';
       res.send({ admin: isAdmin })
-    })
+    });
 
 
     // Load All Users
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
-    })
+    });
 
 
     // Load All Blogs
     app.get("/blogs", async (req, res) => {
       const result = await blogsCollection.find().toArray();
       res.send(result)
-    })
+    });
 
     app.get("/blog/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const blogs = await blogsCollection.findOne(filter);
       res.send(blogs);
-    })
+    });
 
   }
   catch {
